11427575b0feb23f7690efb6a6d348e4f54d3a36
jangala-krupa <jangala.krupa@freshworks.com>
2019-03-14 18:24:31 +0530

    modifications

--- df.py
+++ df.py
@@ -30,8 +30,7 @@ def list_intents(project_id):
         print('Output contexts:')
         for output_context in intent.output_contexts:
             print('\tName: {}'.format(output_context.name))
-def create_intent(project_id, display_name, training_phrases_parts,
-                  message_texts):
+def create_intent(project_id, display_name, training_phrases_parts,message_texts):
     """Create an intent of the given intent type."""
     import dialogflow_v2 as dialogflow
     try :
@@ -40,42 +39,48 @@ def create_intent(project_id, display_name, training_phrases_parts,
       parent = intents_client.project_agent_path(project_id)
       training_phrases = []
       for training_phrases_part in training_phrases_parts:
-        part = dialogflow.types.Intent.TrainingPhrase.Part(
-            text=training_phrases_part)
-        # Here we create a new training phrase for each provided part.
+        part = dialogflow.types.Intent.TrainingPhrase.Part(text=training_phrases_part)
         training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
       text = dialogflow.types.Intent.Message.Text(text=message_texts)
       message = dialogflow.types.Intent.Message(text=text)
 
       intent = dialogflow.types.Intent(
-        display_name=display_name,
-        training_phrases=training_phrases,
+        display_name=display_name,training_phrases=training_phrases,
         messages=[message])
-
-      response = intents_client.create_intent(parent, intent)
-      parent_followup_intent_name = "testing intent1"
-      print('Intent created: {}'.format(response))
-      #print ("follow up intent is{}".format(parent_followup_intent_name))
-   # print training_phrases
+      parent_response = intents_client.create_intent(parent, intent)
+      training_phrases_parts1= ["yes","yep","ok","sure"]
+      training_phrases1 = []
+      for training_phrases_part1 in training_phrases_parts1:
+        part1 = dialogflow.types.Intent.TrainingPhrase.Part(text=training_phrases_part1)
+        training_phrase1 = dialogflow.types.Intent.TrainingPhrase(parts=[part1])
+        training_phrases1.append(training_phrase1)
+      message_texts1=["ok thanks"]
+      text1 = dialogflow.types.Intent.Message.Text(text=message_texts1)
+      message1 = dialogflow.types.Intent.Message(text=text)    
+      intent = dialogflow.types.Intent(
+        display_name="book-follow_up-intent_yes",training_phrases=training_phrases1,
+        messages=[message1],parent_followup_intent_name= parent_response.name)
+      response1 = intents_client.create_intent(parent, intent)
+      training_phrases_parts2= ["no","nope","not","not now"]
+      training_phrases2 = []
+      for training_phrases_part2 in training_phrases_parts2:
+        part2 = dialogflow.types.Intent.TrainingPhrase.Part(text=training_phrases_part2)
+        training_phrase2 = dialogflow.types.Intent.TrainingPhrase(parts=[part2])
+        training_phrases2.append(training_phrase2)
+      message_texts2=["ok thanks see you soon"]
+      text2 = dialogflow.types.Intent.Message.Text(text=message_texts2)
+      message2 = dialogflow.types.Intent.Message(text=text)    
+      intent = dialogflow.types.Intent(
+        display_name="book-follow_up-intent_no",training_phrases=training_phrases2,
+        messages=[message2],parent_followup_intent_name= parent_response.name)
+      response2 = intents_client.create_intent(parent, intent)
+      
+      print('Intent created: {}'.format(parent_response, response1, response2))
     except Exception as e:
-       print ("Intent already exists!")
+       print ("Intent already exists!", e)
 if choice == 1:
  display_name = raw_input("Enter the intent name!")
- print ("enter number of training phrases")
- n = eval(raw_input())
- print ("enter the training phrases")
- tp_parts = []
- for tp in range(1,n+1):
-  part = raw_input()
-  tp_parts.append(part)
- print ("enter the number of responses")
- m = eval(raw_input())
- re_parts = []
- print ("enter the responses")
- for re in range(1,m+1):
-  repart = raw_input()
-  re_parts.append(repart)
- create_intent(project_id,display_name, training_phrases_parts=tp_parts, message_texts=re_parts)
+ create_intent(project_id,display_name, training_phrases_parts=["buy a ticket","book a ticket"], message_texts=["ok thanks"])
 elif choice == 2:
   list_intents(project_id)
